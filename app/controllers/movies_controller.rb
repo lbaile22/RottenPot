313dcf0b9ecc8ad717bf4aed914115602dcd0d91
saasbook <saasbook@saasbook.(none)>
2014-10-13 18:28:27 -0300

    4.10

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -34,4 +34,12 @@ def destroy
   flash[:notice] = "Movie '#{@movie.title}' deleted."
   redirect_to movies_path
 end
+def update
+  @movie = Movie.find params[:id]
+  @movie.update_attributes!(params[:movie])
+  respond_to do |client_wants|
+    client_wants.html {  redirect_to movie_path(@movie)  } # as before
+    client_wants.xml  {  render :xml => @movie.to_xml    }
+  end
+end
 end
