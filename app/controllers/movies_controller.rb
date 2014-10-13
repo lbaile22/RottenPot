d22fe70370374c335cd5601d3cbd791eca97f956
saasbook <saasbook@saasbook.(none)>
2014-10-13 17:56:38 -0300

    4.7

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,4 +13,9 @@ end
 def new
 	# default: render 'new' template
 end
+def create
+@movie = Movie.create!(params[:movie])
+flash[:notice] = "#{@movie.title} was successfully created."
+	redirect_to_movies_path
+end
 end
